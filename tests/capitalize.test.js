29a7c701b9fb691b780b8944e0e9ada45fd1ce89
Valeria Krasnykh <valerie-kras@mail.ru>
2021-04-07 12:20:05 +0300

    Тесты переписаны с использованием strictEqual

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,10 +1,14 @@
+import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
+/* if (capitalize('hello') !== 'Hello') {
 	throw new Error('Функция работает неверно');
 }
 
 if (capitalize('') !== '') {
 	throw new Error('Функция работает неверно');
 }
-console.log('Тесты пройдены');
+console.log('Тесты пройдены'); */
+
+assert.strictEqual(capitalize('hello'), 'Hello');
+assert.strictEqual(capitalize(''), '');
